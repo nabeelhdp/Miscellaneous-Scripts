33a0a78d8036a238d2cce87bb9f65497326d48b8
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2019-09-17 19:10:03 +0800

    Update ansible_set_ip.py

--- ansible_set_ip.py
+++ ansible_set_ip.py
@@ -1,43 +1,46 @@
+#!/usr/bin/python
+
 import sys
 
-inputfile = 'inputfile.txt'
-outfile_name = sys.argv[1]
-service_name = sys.argv[2]
-role_type = sys.argv[3]
+inputfile = sys.argv[1]
+outfile_name = sys.argv[2]
+service_name = sys.argv[3]
+role_type = sys.argv[4]
 service_template = "{}_template.txt".format(service_name)
-
+section_content={}
 sections = []
 
 with open(inputfile) as f:
-   lines = f.read().strip()
+  inputlines_ = f.read().strip()
 
-ip=lines.split("\n")
-st_out=""
-mas_out=""
-sla_out=""
+# Remove empty element in list \
+# may get generated at end due to trailing newline
+# or due to other issues
+inputlines = filter( None, inputlines_.split("\n"))
 
 with open(service_template) as f:
-   sections = f.read().strip().split("\n").strip()
+    sections_ = f.read().strip().lower().split("\n")
+    # Remove empty element in list \
+    # may get generated at end due to trailing newline
+    # or due to other issues
+    sections =  filter(None, sections_)
+    for section in sections:
+      section_content[section]=[]
 
-if role_type == 'standalone' :
-    if len(ip) == 1 :
-        st_out=ip[0]
-    if len(ip) > 1 :
-        mas_out=ip[0]
-        sla_out="\n".join(ip[1:])
-
-if role_type == 'non-standalone' :
-    st_out=ip[0]
-    mas_out=ip[1]
-    sla_out="\n".join(ip[2:])
-
+for inputline in inputlines:
+    type=inputline.split(" ")[-1].split("=")[1]
+    section_content[type].append(inputline.split(" ")[:-1])
+    
 with open(outfile_name,'at') as out:
-    try:
-        out.write('{}\n{}\n'.format(sections[0],st_out))
-        out.write('{}\n{}\n'.format(sections[1],mas_out))
-        out.write('{}\n{}\n'.format(sections[2],sla_out))
+   try:
+       for s in section_content:
+           if len(section_content[s])>0:
+               out.write('{}\n'.format(s))
+               for entries in section_content[s]:
+                   out.write('{}\n'.format(entries))
 
-    finally:
-        out.close()
+
+   finally:
+       out.close()
 
 print(open(sys.argv[1],'rt').read())
